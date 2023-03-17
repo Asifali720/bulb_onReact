fff8622d830bc0668254438576ca9bde9eed97c9
Asif Ali <asiflashari720@gmail.com>
2023-03-17 20:52:17 +0500

    bulb react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import imageOff from './bulb.png'
+import imageOn from './light-bulb.png'
+import React, { useState } from "react";
 
 function App() {
+ const [toggle, setToggle] = useState(false)
+ const [background, setBackground] = useState(true)
+
+ const  handleOnClickToggle = () =>{
+   setToggle(!toggle)
+   setBackground(!background)
+   
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <div style={{backgroundColor: background?'black':'#ffc30a', height: '100vh'}}>
+      <img src={toggle === true?imageOn:imageOff} alt="" />
+      <div style={{backgroundColor: background?'black':'#ffc30a' }}>
+        <button style={{ borderRadius: '5px', backgroundColor: 'green', padding: '10px', border: 'none', color: 'white', position: 'absolute', left: '50%', transform: 'translate(-50%)'}} onClick={handleOnClickToggle}>{toggle?'Bulb off':'Bulb on'}</button>
+      </div>
+      </div>  
     </div>
   );
 }
